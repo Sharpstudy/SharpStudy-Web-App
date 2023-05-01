926aa2c25c3f043307907a02fe62fee1ee345d3b
Mike1 Qureshi <mustafashakeel@gmail.com>
2023-05-01 12:33:27 -0700

    removed the vercel url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,4 @@
 const baseUrl =
-	process.env.NODE_ENV === "production"
-		? "https://sharp-study-web-app.vercel.app"
-		: "http://localhost:3000";
+	process.env.NODE_ENV === "production" && "http://localhost:3000";
 
 export default baseUrl;
