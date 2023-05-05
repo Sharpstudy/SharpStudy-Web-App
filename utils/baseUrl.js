ae8e02353fc5c498e5a0ef1e6b1acd6ad580a6f4
Micah J <justmic007@gmail.com>
2023-05-05 18:16:55 +0100

    test base urls

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "sharpstudy-web-app-load-balancer-1601079994.ca-central-1.elb.amazonaws.com"
+    ? "http://sharpstudy-web-app-load-balancer-1601079994.ca-central-1.elb.amazonaws.com"
     // ? "http://localhost:3000"
     : "http://localhost:3000";
     // : "sharpstudy-web-app-load-balancer-1601079994.ca-central-1.elb.amazonaws.com";
