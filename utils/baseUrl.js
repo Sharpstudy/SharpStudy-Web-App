5644d780b30cb465be28c435234070774bf9ea34
Micah J <justmic007>
2024-01-05 03:58:39 +0100

    config changes

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  process.env.NODE_ENV === "produuction" ? "http://localhost:3000" : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? "https://sharpstudy.io" : "http://localhost:3000";
 
 export default baseUrl;
 
