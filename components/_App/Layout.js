eba8649f0aa5df4ac77152f99927a6555a3f87d3
harlyon <harrisonekpobimi@gmail.com>
2024-01-24 14:49:57 +0200

    feat/Add google analytics tracking

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -9,63 +9,72 @@ import axios from "axios";
 import baseUrl from "@/utils/baseUrl";
 
 const Layout = ({ children }) => {
-	const [modalOpen, setModalOpen] = useState();
-	const [modalImage, setModalImage] = useState("");
+  const [modalOpen, setModalOpen] = useState();
+  const [modalImage, setModalImage] = useState("");
 
-	useEffect(() => {
-		const fetchSetting = async () => {
-			const resp = await axios.get(`${baseUrl}/settings.json`);
-			setModalOpen(resp.data.siteModal);
-			setModalImage(resp.data.siteModalImage);
-			if (resp.data.siteModal) {
-				document.body.style.overflow = "hidden";
-			}
-		};
-		fetchSetting();
-	}, []);
+  useEffect(() => {
+    const fetchSetting = async () => {
+      const resp = await axios.get(`${baseUrl}/settings.json`);
+      setModalOpen(resp.data.siteModal);
+      setModalImage(resp.data.siteModalImage);
+      if (resp.data.siteModal) {
+        document.body.style.overflow = "hidden";
+      }
+    };
+    fetchSetting();
+  }, []);
 
-	const close = () => {
-		setModalOpen(false);
-		document.body.style.overflow = "unset";
-	};
+  const close = () => {
+    setModalOpen(false);
+    document.body.style.overflow = "unset";
+  };
 
-	return (
-		<>
-			<Head>
-				<title>SharpStudy - Online learning | Skills</title>
-				<meta
-					name="viewport"
-					content="initial-scale=1.0, width=device-width"
-				/>
-				<link
-					rel="icon"
-					href="/images/favicon.ico"
-				/>
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>SharpStudy - Online learning | Skills</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
 
-			{children}
+      {children}
 
-			<Script src="https://meet.jit.si/external_api.js" />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-QQHKTTTD86"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-QQHKTTTD86', {
+							page_path: window.location.pathname,
+						});
+					`,
+        }}
+      />
 
-			<Toaster />
+      <Script src="https://meet.jit.si/external_api.js" />
 
-			<GoTop />
+      <Toaster />
 
-			<AnimatePresence
-				initial={false}
-				onExitComplete={() => null}
-				mode="wait"
-			>
-				{modalOpen && modalImage && (
-					<Modal
-						modalOpen={modalOpen}
-						handleClose={close}
-						image_url={modalImage}
-					/>
-				)}
-			</AnimatePresence>
-		</>
-	);
+      <GoTop />
+
+      <AnimatePresence initial={false} onExitComplete={() => null} mode="wait">
+        {modalOpen && modalImage && (
+          <Modal
+            modalOpen={modalOpen}
+            handleClose={close}
+            image_url={modalImage}
+          />
+        )}
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default Layout;
