9b42d1db1a89405fc4526a549b5d991aa0428a2d
Micah J <justmic007>
2024-02-19 15:44:27 +0100

    removed needless lines of code

--- pages/api/swagger.js
+++ pages/api/swagger.js
@@ -1,8 +1,4 @@
-// pages/api/swagger.js
-
 import swaggerJSON from './swagger.json';
-
-// console.log('swaggerJSON', swaggerJSON);
 
 export default function handler(req, res) {
   res.status(200).json(swaggerJSON);
--- pages/swagger.js
+++ pages/swagger.js
@@ -1,15 +1,3 @@
-// pages/swagger.js
-
-// import React from 'react';
-// import SwaggerUI from 'swagger-ui-react';
-// import 'swagger-ui-react/swagger-ui.css';
-
-// const SwaggerPage = () => {
-//   return <SwaggerUI url="/swagger" />;
-// };
-
-// export default SwaggerPage;
-
 import React from 'react';
 import SwaggerPage from '../components/SwaggerPage/SwaggerPage';
 
