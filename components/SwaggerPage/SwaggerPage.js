cd8c2ec4235e87d2149785881fc95836360118f5
Micah J <justmic007>
2024-02-23 14:41:09 +0100

    code refactor and added authorization

--- components/SwaggerPage/SwaggerPage.js
+++ components/SwaggerPage/SwaggerPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import swaggerJSON from '../../pages/api/swagger.json';
 
 const SwaggerPage = () => {
-  return <SwaggerUI url="/api/swagger" />;
+  const securityDefinitions = {
+    BearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+    },
+  };
+
+  return <SwaggerUI spec={swaggerJSON} securityDefinitions={securityDefinitions} />;
 };
 
 export default SwaggerPage;
