060e214ad9ee4b625d9e03ebe7f0317126682150
Micah J <justmic007>
2024-05-05 08:09:51 +0100

    quiz service

---
+++ pages/api/quizzes/create.js
@@ -0,0 +1,75 @@
+import { slugify } from "@/utils/auth";
+import { Course, Quiz } from "database/models";
+
+export default async function handler(req, res) {
+  console.log('req.body');
+  if (!("authorization" in req.headers)) {
+    return res.status(401).json({ message: "No autorization token" });
+  }
+  switch (req.method) {
+    case "POST":
+      await handlePostRequest(req, res);
+      break;
+    case "DELETE":
+      await handleDeleteRequest(req, res);
+      break;
+    default:
+      res.status(405).json({
+        message: `Method ${req.method} not allowed`,
+      });
+  }
+}
+
+const handlePostRequest = async (req, res) => {
+  const {
+    title,
+    description,
+    courseId
+  } = req.body;
+  try {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+      // Token comes from the UI or Swagger docs
+      token = req.headers.authorization.split(' ')[1];
+
+    } else if (req.headers.authorization) {
+      // Token comes without the "Bearer " prefix
+      token = req.headers.authorization;
+    } else {
+      return res.status(401).json({ message: "Invalid or missing authorization header" });
+    }
+
+    // Verify the token using the extracted token variable
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+
+    let slug = slugify(title);
+    const slugExist = await Quiz.findOne({
+      where: { slug: slug },
+    });
+
+    if (slugExist) {
+      slug = `${slug}-${Math.floor(
+        Math.random() * (999 - 100 + 1) + 100
+      )}`;
+    }
+
+    const newQuiz = await Quiz.create({
+      title,
+      slug,
+      description,
+      courseId,
+      userId
+    });
+
+    res.status(200).json({
+      message:
+        "Quiz created successfully.",
+      quiz: newQuiz,
+    });
+  } catch (e) {
+    res.status(400).json({
+      error_code: "create_quiz",
+      message: e.message,
+    });
+  }
+};
