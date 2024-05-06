ebcd4527a493f45460aaaf1802e61f1916590a0f
Micah J <justmic007>
2024-05-06 13:06:36 +0100

    reusable auth verification code

--- pages/api/quizzes/create.js
+++ pages/api/quizzes/create.js
@@ -1,5 +1,4 @@
-import jwt from 'jsonwebtoken';
-import { slugify } from "@/utils/auth";
+import { slugify, verifyUser } from "@/utils/auth";
 import { Quiz } from "database/models";
 
 export default async function handler(req, res) {
@@ -27,20 +26,7 @@ const handlePostRequest = async (req, res) => {
     courseId
   } = req.body;
   try {
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-      // Token comes from the UI or Swagger docs
-      token = req.headers.authorization.split(' ')[1];
-
-    } else if (req.headers.authorization) {
-      // Token comes without the "Bearer " prefix
-      token = req.headers.authorization;
-    } else {
-      return res.status(401).json({ message: "Invalid or missing authorization header" });
-    }
-
-    // Verify the token using the extracted token variable
-    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await verifyUser(req, res);
 
     if (user.role === 'student') {
       return res.status(401).json({ message: "User is not authorized to create a quiz" });
