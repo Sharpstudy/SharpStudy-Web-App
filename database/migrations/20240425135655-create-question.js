d4d47cfb76c5490d92453b3ed514f175606593b9
Micah J <justmic007>
2024-05-06 11:07:48 +0100

    update db tables

--- database/migrations/20240425135655-create-question.js
+++ database/migrations/20240425135655-create-question.js
@@ -21,14 +21,16 @@ module.exports = {
           as: "quizId",
         },
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
   async down(queryInterface, Sequelize) {
